6025c1c3a856f735e1b266fcb90dca3776dd89c7
francoquinteros <faquinteros05@gmail.com>
2025-08-15 17:44:18 -0300

    Subida Proyecto Final Mundo Auto

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+(function(){
+  const form = document.querySelector('.needs-validation');
+  if(!form) return;
+  form.addEventListener('submit', (e) => {
+    if (!form.checkValidity()) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    form.classList.add('was-validated');
+  }, false);
+})();
